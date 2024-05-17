9e740ed7cf50a84f7485843e4ee023237edc4136
rajnishfrost <rajnishfrost@gmail.com>
2024-05-17 11:54:07 +0530

    change in network jsx file impoerted navbar

--- src/Pages/Networks/Networks.jsx
+++ src/Pages/Networks/Networks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Navbar from "../../Common Comp/Navbar/Navbar"
 
 export default function Networks() {
     const getNetwork = useSelector((state) => state.network.network.data);
     console.log(getNetwork);
     return (
         <div >
+            <Navbar/>
             <table>
                 <thead>
                     <tr>
